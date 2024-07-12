98fa0e5179d85f55e0af02dbb613788c740f7593
kkren <i@kkren.me>
2024-07-11 22:10:18 -0400

    update dir name

--- generate.sh
+++ generate.sh
@@ -1,11 +1,17 @@
 make_ros() {
-  local file=$1
+  local dir=$1
   local name=$2
   echo "/ip firewall address-list remove [/ip firewall address-list find list=$name]"
   echo "/ip firewall address-list"
   while read line; do
     echo "add address=$line disabled=no list=$name"
   done < $file
+
+  for file in $1/*.txt; do
+    while read line; do
+      echo "add address=$line disabled=no list=$name"
+    done < $file
+  done
 }
 
 # Generate backward Compatibility address-list.rsc
